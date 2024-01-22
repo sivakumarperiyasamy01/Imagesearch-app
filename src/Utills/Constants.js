857de74bf37f13bf5311aa2ee1a228ce5b6b7151
sivakumarperiyasamy01 <sivakumarperiyasamy01@gamil.com>
2024-01-23 00:17:31 +0530

    updateapi

--- src/Utills/Constants.js
+++ src/Utills/Constants.js
@@ -1,4 +1,1 @@
 
-export const API_KEY=
-
-export const PIXAPAY_IMAGE_API="https://pixabay.com/api/?key="+API_KEY+"&q=yellow+flowers&image_type=photo&pretty=true"
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,35 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const ImageCard = ({data}) => {
+const ImageCard = ({ data }) => {
+  const { tags } = data;
 
-  const {tags}=data
-  // console.log(tags)
-  const strarray=tags.split(",")
-  console.log(strarray)
-
+  const strarray = tags.split(",");
   //object destructring
-  const{webformatURL,views,downloads,likes,user}=data
+  const { webformatURL, views, downloads, likes, user } = data;
 
   return (
-    <div className='w-60 h-auto m-4 bg-gray-100 shadow-md p-2'>
-      <img className="" alt="logo" src={webformatURL }></img>
+    <div className="w-60 h-auto m-4 bg-gray-100 shadow-md p-2">
+      <img className="" alt="logo" src={webformatURL}></img>
       <div>
-        <h5 className='text-rose-900 font-bold px-3 my-2 '>Photo By:{user}</h5>
-        <ul className='font-bold text-sm px-3 py-2'>
+        <h5 className="text-rose-900 font-bold px-3 my-2 ">Photo By:{user}</h5>
+        <ul className="font-bold text-sm px-3 py-2">
           <li>Views:{views}</li>
           <li>Downloads:{downloads}</li>
           <li>Likes:{likes}</li>
         </ul>
-        <div className='flex flex-wrap m-2'>
-        <span className='font-bold text-sm px-3 py-1 mx-1 my-2 bg-green-100'>#{strarray[0]}</span>
-        <span className='font-bold text-sm px-3 py-2 mx-1 my-2 bg-green-100'>#{strarray[1]}</span>
-        <span className='font-bold text-sm px-3 py-2 mx-1 my-2 bg-green-100'>#{strarray[2]}</span>
+        <div className="flex flex-wrap m-2">
+          <span className="font-bold text-sm px-3 py-1 mx-1 my-2 bg-green-100">
+            #{strarray[0]}
+          </span>
+          <span className="font-bold text-sm px-3 py-2 mx-1 my-2 bg-green-100">
+            #{strarray[1]}
+          </span>
+          <span className="font-bold text-sm px-3 py-2 mx-1 my-2 bg-green-100">
+            #{strarray[2]}
+          </span>
         </div>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default ImageCard
+export default ImageCard;
